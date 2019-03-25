ff3d772632b1a0c3ce114be97cdd5e9b3f24535d
Ozaki Yoshiaki <kascado.ys10@gmail.com>
2019-03-26 00:43:41 +0900

    Add component MainView

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { render } from 'react-dom';
 import NavBar from './Navbar';
 import styled from '../theme/index';
+import MainView from './MainView';
 
 const Content = styled.div`
   display: flex;
@@ -10,17 +10,13 @@ const Item = styled.div`
   margin: 0px 5px;
 `;
 
-class App extends React.Component<{}> {
-  render() {
-    return (
-      <Content>
-        <NavBar />
-        <Item>
-          <h1>Hello, Electron!!</h1>
-        </Item>
-      </Content>
-    );
-  }
+export default function App() {
+  return (
+    <Content>
+      <NavBar />
+      <Item>
+        <MainView />
+      </Item>
+    </Content>
+  );
 }
-
-export default App;
