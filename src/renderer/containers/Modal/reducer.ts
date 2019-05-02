ee2f86560424bdc3405b45f0d2ccd3dad0376e57
yuta4j1 <kascado.ys10@gmail.com>
2019-05-02 23:09:49 +0900

    update

--- src/renderer/containers/Modal/reducer.ts
+++ src/renderer/containers/Modal/reducer.ts
@@ -1,13 +1,22 @@
+import { TOGGLE_MODAL } from './actionType';
 import { ModalState } from '../../types';
 
-export const ActModalState = (
-  modalState: ModalState,
+const initialState: ModalState = {
+  isOpen: false,
+  modalBody: undefined,
+  atClose: () => console.log('None')
+};
+
+const modalStateReducer = (
+  modalState = initialState,
   action: { type: string; payload: ModalState }
 ): ModalState => {
   switch (action.type) {
-    case 'TOGGLE_MODAL':
-      return action.payload;
+    case TOGGLE_MODAL:
+      return Object.assign({}, action.payload);
     default:
       return modalState;
   }
 };
+
+export default modalStateReducer;
