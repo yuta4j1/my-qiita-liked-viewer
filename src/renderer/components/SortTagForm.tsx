74fb6b778f6c9ea801f718bf87821487767590d2
yuta4j1 <kascado.ys10@gmail.com>
2019-05-02 23:20:15 +0900

    fix: delete props

--- src/renderer/components/SortTagForm.tsx
+++ src/renderer/components/SortTagForm.tsx
@@ -3,19 +3,11 @@ import styled from '../theme/index';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-type Props = {
-  tagList: string[];
-};
-
 const Header = styled.div`
   display: flex;
   flex-direction: row-reverse;
 `;
-class SortTagForm extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
-
+class SortTagForm extends React.Component<{}> {
   render() {
     return (
       <Header>
