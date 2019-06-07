b9dcb2a71d845379d3f39b15c1920004367153c6
yuta4j1 <kascado.ys10@gmail.com>
2019-06-07 16:45:26 +0900

    feat: 'Loading' conteiner component's reducer

--- src/renderer/containers/store.ts
+++ src/renderer/containers/store.ts
@@ -1,7 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import modalStateReducer from './Modal/reducer';
+import loadingStateReducer from './Loading/reducer';
 
-export const rootReducer = combineReducers({ modalStateReducer });
+export const rootReducer = combineReducers({
+  modalStateReducer,
+  loadingStateReducer
+});
 
 export type AppState = ReturnType<typeof rootReducer>;
 
