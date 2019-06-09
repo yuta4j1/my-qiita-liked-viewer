2dc75f3040f35667ed269b73d49ef30609768f2d
yuta4j1 <kascado.ys10@gmail.com>
2019-06-09 18:08:57 +0900

    feat: 'ColumnControl' components to edit MainView's column

---
+++ src/renderer/components/ColumnControl/ColumnCard.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { ColumnCardState } from '../../types';
+import styled from '~/theme';
+
+const Card = styled.div`
+  padding: 5px;
+  margin: 5px;
+  border-radius: 3px;
+  background-color: #f6f6f6;
+`;
+
+type ColumnCardProps = {
+  key: number;
+  card: ColumnCardState;
+};
+
+const ColumnCard: React.FC<ColumnCardProps> = props => {
+  const [card, setCard] = React.useState(props.card);
+
+  return <Card>{card.text}</Card>;
+};
+
+export default ColumnCard;
---
+++ src/renderer/components/ColumnControl/index.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import ColumnCard from './ColumnCard';
+import { ColumnCardState } from '../../types';
+import styled from '~/theme/index';
+
+const Explain = styled.div`
+  margin: 5% 5%;
+`;
+
+const CardTable = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+type ColumnControlProps = {
+  columnNames: string[];
+};
+
+const ColumnControl: React.FC<ColumnControlProps> = props => {
+  const [columnNames, setColumnNames] = React.useState(props.columnNames);
+
+  const generateCard = (): JSX.Element[] => {
+    return columnNames.map((columnName: string, idx: number) => {
+      const card: ColumnCardState = {
+        id: '1',
+        text: columnName,
+        order: idx + 1
+      };
+      return <ColumnCard key={idx} card={card} />;
+    });
+  };
+
+  return (
+    <div>
+      <Explain>
+        // TODO 列の追加、ソートをここでやる
+      </Explain>
+      <CardTable>{generateCard()}</CardTable>
+    </div>
+  );
+};
+
+export default ColumnControl;
