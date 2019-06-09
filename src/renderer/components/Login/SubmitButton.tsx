2764289f5ec7b589776581466b7f12261952911c
yuta4j1 <kascado.ys10@gmail.com>
2019-06-09 23:19:42 +0900

    fix: Login button split into component

---
+++ src/renderer/components/Login/SubmitButton.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styled from '~/theme';
+
+type Props = {
+  clickEvent: () => void;
+};
+
+const Button = styled.button`
+  padding: 12px;
+  height: 10%;
+  width: 16%;
+  cursor: pointer;
+  border-style: none;
+  border-radius: 8px;
+  color: #55c500;
+  font-size: 1.2em;
+  background-color: #fafafa;
+  transition: background-color .3s
+  &:hover {
+    background-color: #dcedc8;
+  }
+`;
+
+const SubmitButton: React.FC<Props> = props => {
+  return <Button onClick={e => props.clickEvent()}>Sign in</Button>;
+};
+
+export default SubmitButton;
