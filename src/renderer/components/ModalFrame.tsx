071ce0c259504d4b11d04eb5f33c58daf3f1bd97
yuta4j1 <kascado.ys10@gmail.com>
2019-06-09 18:07:08 +0900

    fix: props

--- src/renderer/components/ModalFrame.tsx
+++ src/renderer/components/ModalFrame.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Modal from 'react-modal';
 import { ModalState } from '../types';
+import Containts from './ModalContaints';
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
+    top: '50vh',
+    left: '50vw',
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
@@ -15,20 +16,39 @@ const customStyles = {
 
 type ModalFrameProps = {
   currentState: ModalState;
+  closeModal?: (state: ModalState) => void;
 };
 
-const ModalFrame: React.FC<ModalFrameProps> = props => {
-  return (
-    <Modal
-      isOpen={props.currentState.isOpen}
-      onRequestClose={props.currentState.atClose}
-      shouldCloseOnOverlayClick={true}
-      style={customStyles}
-    >
-      {props.currentState.modalBody}
-    </Modal>
-  );
-};
+class ModalFrame extends React.Component<ModalFrameProps> {
+  constructor(props) {
+    super(props);
+  }
+
+  onClose() {
+    this.props.closeModal({
+      isOpen: false,
+      modalBody: <div />,
+      atClose: () => console.log('')
+    });
+  }
+
+  render() {
+    return (
+      <Modal
+        isOpen={this.props.currentState.isOpen}
+        onRequestClose={this.props.currentState.atClose}
+        shouldCloseOnOverlayClick={true}
+        style={customStyles}
+      >
+        <Containts
+          headerTitle={'hoge'}
+          body={this.props.currentState.modalBody}
+          closeEvent={() => this.onClose()}
+        />
+      </Modal>
+    );
+  }
+}
 
 Modal.setAppElement('#app');
 
