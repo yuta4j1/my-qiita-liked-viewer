c9b93c36f3db68c4b7fb58e3829321b2bc0c032a
yuta4j1 <kascado.ys10@gmail.com>
2019-06-09 23:17:46 +0900

    fix: rename component 'InputUid' -> 'InputUidText'

---
+++ src/renderer/components/Login/InputUidText.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import styled from '~/theme';
+
+type Props = {
+  text: string;
+  changeEvent: (string) => void;
+};
+
+const Input = styled.input`
+  padding: 6px;
+  width: 30%;
+  height: 30px;
+  border-radius: 8px;
+  border-style: none;
+  font-family: 'Assistant', sans-serif;
+  font-size: 20px;
+`;
+
+const InputUidText: React.FC<Props> = props => {
+  return (
+    <Input
+      type="text"
+      value={props.text}
+      onChange={e => props.changeEvent(e.target.value)}
+    />
+  );
+};
+
+export default InputUidText;
