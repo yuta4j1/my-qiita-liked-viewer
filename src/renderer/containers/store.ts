70ef560aab7b1c7651b488a51304d9b2b6536cd3
yuta4j1 <kascado.ys10@gmail.com>
2019-06-10 22:26:50 +0900

    feat: loginActionReducer

--- src/renderer/containers/store.ts
+++ src/renderer/containers/store.ts
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import modalStateReducer from './Modal/reducer';
 import loadingStateReducer from './Loading/reducer';
+import loginActionReducer from './Login/reducer';
 
 export const rootReducer = combineReducers({
   modalStateReducer,
-  loadingStateReducer
+  loadingStateReducer,
+  loginActionReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
