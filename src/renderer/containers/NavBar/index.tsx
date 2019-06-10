4f1abb44cc6d3d13c7e540b76a68123dd3fb332a
yuta4j1 <kascado.ys10@gmail.com>
2019-06-10 22:23:37 +0900

    feat: mapStateToProps (get user info from store)

--- src/renderer/containers/NavBar/index.tsx
+++ src/renderer/containers/NavBar/index.tsx
@@ -1,16 +1,22 @@
-import NavBar from '../../components/NavBar';
-import { ModalState } from '../../types';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
+import NavBar from '~/components/NavBar';
+import { ModalState } from '../../types';
 import { toggleModal } from '../Modal/action';
+import { AppState } from '../store';
 
+const mapStateToProps = (state: AppState) => {
+  return {
+    signinUser: state.loginActionReducer
+  };
+};
 const mapDispatchToProps = (dispatch: Dispatch, ownProps: ModalState) => {
   return {
-    openModal: ownProps => dispatch(toggleModal(ownProps))
+    toggleModal: ownProps => dispatch(toggleModal(ownProps))
   };
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(NavBar);
